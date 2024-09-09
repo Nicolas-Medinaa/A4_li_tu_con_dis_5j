90c1ecb0eb3b5181bab44b650a412d2106e52cf2
Nicolas Medina Zubia <a.22308051281259@cbtis128.edu.mx>
2024-09-09 10:13:44 -0600

    Add files via upload

---
+++ mi_lista.py
@@ -0,0 +1,18 @@
+
+print ("ejemplo de lista")
+arcoiris=["verde","azul","morado"]
+print (arcoiris)
+longitud=len(arcoiris)
+print ("Elementos que comtiene la lista arcoiris", longitud)
+print (f"Elementos que comtiene la lista arcoiris {longitud}")
+print ("accediendo a un elemento de la lista")
+print (arcoiris[1])
+print (f"elemento de la posicion cero es {arcoiris[0]}")
+print (f"el primer color del arcoiris es: ",arcoiris[0])
+print ("Agregar un elemento de la lista")
+print (arcoiris)
+arcoiris.append("naranja")
+print (arcoiris)
+for nicolass in arcoiris:
+    print (nicolass)
+    
---
+++ mi_tuplas.py
@@ -0,0 +1,8 @@
+print ("Ejemplos de tuplas")
+canciones = ("Te quiero","El Noa Noa","Amor Tumbado")
+print(canciones)
+y = list(canciones)
+print(y)
+y[1] = "La people II"
+x = tuple(y)
+print(x)
