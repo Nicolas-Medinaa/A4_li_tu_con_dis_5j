395ddfc251d4c18f11327afe2f013dd54d22aa23
Nicolas Medina Zubia <a.22308051281259@cbtis128.edu.mx>
2024-09-11 07:49:05 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,7 @@
+print("Ejemplo de Conjunto set")
+tipodeportes={"futbol","Tenis","basquetball"}
+print(tipodeportes)
+print("Lista de elementos del conjunto tipodeportes loop for")
+print(tipodeportes)
+for medina in tipodeportes:
+    print(medina)
